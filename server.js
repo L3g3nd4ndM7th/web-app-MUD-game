de46c70246a0167b23848423506996e74c1cfe5f
l3g3nd4ndm7th <l3g3nd4ndm7th@gmail.com>
2023-07-15 14:53:05 -0400

    optimized and shortened the code

--- server.js
+++ server.js
@@ -1,164 +1,114 @@
 const WebSocket = require('ws');
 const fs = require('fs');
-const path = require('path');
-
-const logsFolder = path.join(__dirname, 'server logs');
-const databaseFolder = path.join(__dirname, 'database');
-
-const loginsFilePath = path.join(databaseFolder, 'logins.json');
-const messagesFilePath = path.join(logsFolder, 'messages.txt');
-const connectionsFilePath = path.join(logsFolder, 'connections.txt');
-const disconnectionsFilePath = path.join(logsFolder, 'disconnections.txt');
-const errorsFilePath = path.join(logsFolder, 'errors.txt');
-
 const wss = new WebSocket.Server({ port: 8080 });
-const speciesOptions = ['human', 'elf', 'dwarf', 'halfling', 'halfgiant'];
-const archetypeOptions = ['fighter', 'rogue', 'mage'];
-const alignmentOptions = ['lawful', 'neutral', 'chaotic'];
-const stats = ['strength', 'constitution', 'dexterity', 'intelligence', 'wisdom', 'charisma'];
 
-const inputModes = ['justConnected', 'loginUsername', 'loginPassword', 'loginPasswordConfirm', 'registerUsername', 'registerUsernameConfirm',
-    'registerPassword', 'registerPasswordConfirm', 'chCrSpecies', 'chCrArchetype', 'chCrAlignment', 'chCrAbilityScores',
-    'chCrMoney', 'chCrName', 'mainGame'];
-
-const commands = {
-    'a': 'attack',
-    'l': 'login',
-    'r': 'register',
-    'lk': 'look',
-    'mv': 'move',
-    'i': 'item',
-    'u': 'use',
-    'ex': 'examine',
-    'eq': 'equip',
-    'uneq': 'unequip',
-    'ch': 'character'
-};
-
-// let clients = [];
-// let clientAddress = // netaddress and netport
-// let currentTimeStamp = //new Date();
-// let usernameAttempts = 0;
-// let passwordAttempts = 0;
-let tempLoginUsername = ''
-let tempLoginPassword = ''
-let tempRegisterUsername = ''
-let tempRegisterPassword = ''
-
-let character = {
-    name: '',
-    species: '', // human, elf, dwarf, halfling, halfgiant, beastkin, draconic, mutant
-    archetype: '', // fighter, rogue, mage
-    alignment: '', // lawful, neutral, chaotic
-    strength: 0,
-    constitution: 0,
-    dexterity: 0,
-    intelligence: 0,
-    wisdom: 0,
-    charisma: 0,
-    money: 0,
-    level: 1,
-    experience: 0
-};
-
-let user = {
-    netaddress: '',
-    netport: '',
-    username: '',
-    inputMode: 'justConnected',
-    characterName: '',
-    abilityScoresRerolls: 3,
-    timeAbilityScoresReroll: 0 // sets to 24 hours to restore 1 ability score reroll after they use their 3 ability score rerolls
-};
-
-let logins = []; // Initialize the logins array
-
-// Load user logins from logins.json
+let clients = {};
+let logins = {};
 try {
-    const loginsData = fs.readFileSync(loginsFilePath, 'utf8');
+    const loginsData = fs.readFileSync('database/logins.json', 'utf8');
     logins = JSON.parse(loginsData);
-
-    // Convert the logins data to an array of objects
-    logins = Object.entries(logins).map(([username, password]) => ({ username, password }));
 } catch (error) {
     console.error('Error loading logins:', error);
 }
 
-// Main server listener
-wss.on('connection', (ws) => {
-    console.log('A new client connected.');
+wss.on('connection', (ws, req) => {
+    const clientAddress = req.socket.remoteAddress + ':' + req.socket.remotePort;
+    console.log(`${clientAddress} has connected.`);
+
+    clients[clientAddress] = {
+        username: '',
+        inputMode: 'loginOrRegister',
+        characterName: '',
+        abilityScoresRerolls: 3,
+        timeAbilityScoresReroll: 0,
+    };
+
+    console.log('Number of connected clients:', Object.keys(clients).length);
 
     ws.on('message', (message) => {
-        message = message.toString().trim()
+        const clientAddress = ws._socket.remoteAddress + ':' + ws._socket.remotePort;
+        message = message.toString().trim();
         console.log(`Received message: ${message}`);
 
-        if (message === 'l' && user.inputMode === 'justConnected') {
-            user.inputMode = 'loginUsername';
+        const { inputMode } = clients[clientAddress];
+
+        if (message === 'l' && inputMode === 'loginOrRegister') {
+            clients[clientAddress].inputMode = 'loginUsername';
             ws.send('color-white: Please enter your username.');
-        }
-
-        else if (message === 'r' && user.inputMode === 'justConnected') {
-            user.inputMode = 'registerUsername';
+        } 
+        
+        else if (message === 'r' && inputMode === 'loginOrRegister') {
+            clients[clientAddress].inputMode = 'registerUsername';
             ws.send('color-white: Please register your username.');
-        }
-
-        else if (user.inputMode === 'loginUsername') {
-            if (checkUserExists(message)) {
-                tempLoginUsername = message;
-                user.inputMode = 'loginPassword';
+        } 
+        
+        else if (inputMode === 'loginUsername') {
+            const tempLoginUsername = message;
+            if (logins[tempLoginUsername]) {
+                clients[clientAddress].inputMode = 'loginPassword';
+                clients[clientAddress].username = tempLoginUsername;
                 ws.send('color-white: Please enter your password.');
             } else {
                 ws.send('color-white: User not found.');
             }
-        }
-
-        else if (user.inputMode === 'loginPassword') {
-            tempLoginPassword = message;
-            if (checkCredentials(tempLoginUsername, tempLoginPassword)) {
-                user.username = tempLoginUsername;
-                tempLoginUsername = '';
-                tempLoginPassword = '';
-                ws.send('color-white: Login successful.')
-
-                if (!user.character) {
-                    user.inputMode = 'chCrSpecies';
-                    ws.send(`color-white: It's your first time loggin in. Please create a character.`);
-                    ws.send('color-white: Begin by choosing your character species.');
-                } else {
-                    user.inputMode = 'mainGame';
-                    ws.send('color-white: Welcome to the game world!');
-                }
+        } 
+        
+        else if (inputMode === 'loginPassword') {
+            const tempLoginPassword = message;
+            if (logins[clients[clientAddress].username]?.password === tempLoginPassword) {
+                clients[clientAddress].inputMode = 'chCrSpecies';
+                ws.send(`color-white: It's your first time logging in. Please create a character.`);
+                ws.send('color-white: Begin by choosing your character species.');
+            } else {
+                ws.send('color-white: Passwords do not match. Try again.');
+                clients[clientAddress].inputMode = 'loginPassword';
+                ws.send('color-white: Please enter your password.');
             }
-        }
-
-        else if (user.inputMode === 'registerUsername') {
-            tempRegisterUsername = message;
-            user.inputMode = 'registerPassword';
+        } 
+        
+        else if (inputMode === 'registerUsername') {
+            const tempRegisterUsername = message;
+            if (logins[tempRegisterUsername]) {
+                ws.send('color-white: Username already exists. Please choose a different username.');
+                return;
+            }
+            clients[clientAddress].inputMode = 'registerPassword';
+            clients[clientAddress].tempRegisterUsername = tempRegisterUsername;
             ws.send('color-white: Please register your password.');
-        }
-
-        else if (user.inputMode === 'registerPassword') {
-            tempRegisterPassword = message;
-            user.inputMode = 'registerPasswordConfirm';
+        } 
+        
+        else if (inputMode === 'registerPassword') {
+            const tempRegisterPassword = message;
+            clients[clientAddress].inputMode = 'registerPasswordConfirm';
+            clients[clientAddress].tempRegisterPassword = tempRegisterPassword;
             ws.send('color-white: Please confirm your password.');
-        }
-
-        else if (user.inputMode === 'registerPasswordConfirm') {
+        } 
+        
+        else if (inputMode === 'registerPasswordConfirm') {
+            const { tempRegisterUsername, tempRegisterPassword } = clients[clientAddress];
             if (message === tempRegisterPassword) {
-                createUser(tempRegisterUsername, tempRegisterPassword);
-                tempRegisterUsername = '';
-                tempRegisterPassword = '';
+                logins[tempRegisterUsername] = {
+                    netAddresses: logins[tempRegisterUsername]?.netAddresses || [],
+                    password: tempRegisterPassword,
+                };
+                logins[tempRegisterUsername].netAddresses.push(clientAddress);
+                fs.writeFileSync('database/logins.json', JSON.stringify(logins), 'utf8');
+                clients[clientAddress].tempRegisterUsername = '';
+                clients[clientAddress].tempRegisterPassword = '';
+                clients[clientAddress].inputMode = 'loginOrRegister'
                 ws.send('color-white: Registration successful. Proceed to login.');
             } else {
                 ws.send('color-white: Passwords do not match. Try again.');
-                user.inputMode = 'registerPassword';
+                clients[clientAddress].inputMode = 'registerPassword';
                 ws.send('color-white: Please register your password.');
             }
         }
     });
 
     ws.on('close', () => {
-        console.log('A client disconnected.');
+        console.log(`${clientAddress} disconnected.`);
+        delete clients[clientAddress];
+        console.log('Number of connected clients:', Object.keys(clients).length);
     });
 
     ws.on('error', (error) => {
@@ -167,31 +117,3 @@ wss.on('connection', (ws) => {
 });
 
 console.log('Server is online.');
-
-// Function definitions
-
-// Function to check if the username exists in logins
-function checkUserExists(username) {
-    for (const login of logins) {
-        if (login.username === username) {
-            return true;
-        }
-    }
-    return false;
-}
-
-function checkCredentials(username, password) {
-    const user = logins.find((login) => login.username === username);
-    return user && user.password === password;
-}
-
-function createUser(username, password) {
-    const newUser = { username, password };
-    logins.push(newUser);
-    saveLoginsToFile();
-}
-
-function saveLoginsToFile() {
-    const loginsData = JSON.stringify(logins);
-    fs.writeFileSync(loginsFilePath, loginsData, 'utf8');
-}
