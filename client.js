78baec4d93808851d56956b66f2315f87868a582
l3g3nd4ndm7th <l3g3nd4ndm7th@gmail.com>
2023-07-16 15:05:54 -0400

    encapsulated code for client and server

--- client.js
+++ client.js
@@ -1,63 +1,73 @@
-const socket = new WebSocket('ws://localhost:8080')
+class WebSocketClient {
+  constructor() {
+    this.socket = new WebSocket('ws://localhost:8080');
+    document.addEventListener('keydown', this.handleKeyPress.bind(this));
+    this.socket.addEventListener('message', this.handleMessage.bind(this));
+    this.socket.addEventListener('error', this.handleError.bind(this));
+    this.socket.addEventListener('close', this.handleClose.bind(this));
+  }
 
-socket.addEventListener('message', (event) => {
-  let message = event.data
-  const spanElement = document.createElement('span')
-  const divTextOutput = document.getElementById('div-text-output')
+  handleMessage(event) {
+    let message = event.data;
+    const spanElement = document.createElement('span');
+    const divTextOutput = document.getElementById('div-text-output');
 
-  if (message.includes(':lightpurple:')) {
+    if (message.includes(':lightpurple:')) {
+      spanElement.classList.add('span-color-light-purple');
+      message = message.replace(':lightpurple:', '');
+    }
+
+    if (message.includes(':darkpurple:')) {
+      spanElement.classList.add('span-color-dark-purple');
+      message = message.replace(':darkpurple:', '');
+    }
+
+    if (message.includes(':lightgreen:')) {
+      spanElement.classList.add('span-color-light-green');
+      message = message.replace(':lightgreen:', '');
+    }
+
+    if (message.includes(':darkgreen:')) {
+      spanElement.classList.add('span-color-dark-green');
+      message = message.replace(':darkgreen:', '');
+    }
+
+    spanElement.innerHTML = message;
+    divTextOutput.appendChild(spanElement);
+    divTextOutput.scrollTop = divTextOutput.scrollHeight;
+  }
+
+  handleError(event) {
+    const message = 'Client error.';
+    console.error(event);
+    const spanElement = document.createElement('span');
+    const divTextOutput = document.getElementById('div-text-output');
     spanElement.classList.add('span-color-light-purple');
-    message = message.replace(':lightpurple:', '');
+    spanElement.innerHTML = message;
+    divTextOutput.appendChild(spanElement);
+    divTextOutput.scrollTop = divTextOutput.scrollHeight;
   }
 
-  if (message.includes(':darkpurple:')) {
-    spanElement.classList.add('span-color-dark-purple');
-    message = message.replace(':darkpurple:', '');
+  handleClose(event) {
+    const message = 'Disconnected from the server.';
+    console.log(`Code: ${event.code} Reason: ${event.reason}`);
+    const spanElement = document.createElement('span');
+    const divTextOutput = document.getElementById('div-text-output');
+    spanElement.classList.add('span-color-light-purple');
+    spanElement.innerHTML = message;
+    divTextOutput.appendChild(spanElement);
+    divTextOutput.scrollTop = divTextOutput.scrollHeight;
   }
 
-  if (message.includes(':lightgreen:')) {
-    spanElement.classList.add('span-color-light-green');
-    message = message.replace(':lightgreen:', '');
-  }
+  handleKeyPress(event) {
+    let inputTextMain = document.getElementById('input-text-main');
+    let message = inputTextMain.value.trim();
 
-  if (message.includes(':darkgreen:')) {
-    spanElement.classList.add('span-color-dark-green');
-    message = message.replace(':darkgreen:', '');
-  }
-  
-  spanElement.innerHTML = message
-  divTextOutput.appendChild(spanElement)
-  divTextOutput.scrollTop = divTextOutput.scrollHeight
-})
-
-socket.addEventListener('error', (event) => {
-  const message = `Client error.`;
-  console.error(event)
-  const spanElement = document.createElement('span');
-  const divTextOutput = document.getElementById('div-text-output');
-  spanElement.classList.add('span-color-light-purple');
-  spanElement.innerHTML = message;
-  divTextOutput.appendChild(spanElement);
-  divTextOutput.scrollTop = divTextOutput.scrollHeight;
-});
-
-socket.addEventListener('close', (event) => {
-  const message = `Disconnected from the server.`;
-  console.log(`Code: ${event.code} Reason: ${event.reason}`)
-  const spanElement = document.createElement('span');
-  const divTextOutput = document.getElementById('div-text-output');
-  spanElement.classList.add('span-color-light-purple');
-  spanElement.innerHTML = message;
-  divTextOutput.appendChild(spanElement);
-  divTextOutput.scrollTop = divTextOutput.scrollHeight;
-});
-
-function handleKeyPress(event) {
-  let inputTextMain = document.getElementById('input-text-main');
-  let message = inputTextMain.value.trim();
-
-  if (event.keyCode === 13 || event.key === 'Enter') {
-    socket.send(message);
-    inputTextMain.value = '';
+    if (event.keyCode === 13 || event.key === 'Enter') {
+      this.socket.send(message);
+      inputTextMain.value = '';
+    }
   }
 }
+
+const client = new WebSocketClient();
--- server.js
+++ server.js
@@ -1,175 +1,171 @@
-const WebSocket = require('ws')
-const fs = require('fs')
-const wss = new WebSocket.Server({ port: 8080 })
+const WebSocket = require('ws');
+const fs = require('fs');
 
-let clients = {}
-let logins = {}
+class Server {
+  constructor() {
+    this.clients = {};
+    this.logins = {};
+    this.loginUsernameAttempts = 2;
+    this.loginPasswordAttempts = 2;
+    this.registerPasswordAttempts = 2;
+    this.wss = new WebSocket.Server({ port: 8080 });
 
-let loginUsernameAttempts = 2
-let loginPasswordAttempts = 2
-let registerPasswordAttempts = 2
-
-try {
-    const loginsData = fs.readFileSync('database/logins.json', 'utf8')
-    logins = JSON.parse(loginsData)
-} catch (error) {
-    console.error('Error loading logins:', error)
-}
-
-wss.on('connection', (ws, req) => {
-    const clientAddress = req.socket.remoteAddress + ':' + req.socket.remotePort
-    console.log(`${clientAddress} has connected.`)
-    ws.send(':lightpurple: Connected to the server.')
-    ws.send(':lightpurple: Please login or register.')
-
-    clients[clientAddress] = {
-        username: '',
-        inputMode: 'loginOrRegister',
-        characterName: '',
-        abilityScoresRerolls: 3,
-        timeAbilityScoresReroll: 0,
+    try {
+      const loginsData = fs.readFileSync('database/logins.json', 'utf8');
+      this.logins = JSON.parse(loginsData);
+    } catch (error) {
+      console.error('Error loading logins:', error);
     }
 
-    console.log('Number of connected clients:', Object.keys(clients).length)
+    this.wss.on('connection', (ws, req) => {
+      this.handleConnection(ws, req);
+    });
+
+    console.log('Server is online.');
+  }
+
+  handleConnection(ws, req) {
+    const clientAddress = req.socket.remoteAddress + ':' + req.socket.remotePort;
+    console.log(`${clientAddress} has connected.`);
+    ws.send(':lightpurple: Connected to the server.');
+    ws.send(':lightpurple: Please login or register.');
+
+    this.clients[clientAddress] = {
+      username: '',
+      inputMode: 'loginOrRegister',
+      characterName: '',
+      abilityScoresRerolls: 3,
+      timeAbilityScoresReroll: 0,
+    };
+
+    console.log('Number of connected clients:', Object.keys(this.clients).length);
 
     ws.on('message', (message) => {
-        const clientAddress = ws._socket.remoteAddress + ':' + ws._socket.remotePort
-        const { inputMode } = clients[clientAddress]
-        message = message.toString().trim()
-        console.log(`Received message: ${message}`)
-        
-        if (inputMode === 'loginOrRegister') {
-            if (message === 'l') {
-                clients[clientAddress].inputMode = 'loginUsername'
-                ws.send('Please enter your username.')
-            } 
-            
-            else if (message === 'r') {
-                clients[clientAddress].inputMode = 'registerUsername'
-                ws.send('Please register your username.')
-            } 
-            
-            else {
-                ws.send('Type l or r to login or register.')
-            }
-        }
-        
-        if (inputMode === 'loginUsername') {
-            const tempLoginUsername = message
-
-            if (logins[tempLoginUsername]) {
-                clients[clientAddress].inputMode = 'loginPassword'
-                clients[clientAddress].username = tempLoginUsername
-                ws.send('Please enter your password.')
-            } 
-
-            else {
-                if (loginUsernameAttempts === 0) {
-                    clients[clientAddress].inputMode = 'loginOrRegister'
-                    ws.send(':lightpurple: Login failed. Please try again.')
-                    loginUsernameAttempts = 2
-                }
-
-                else {
-                    ws.send('User not found.')
-                    --loginUsernameAttempts
-                }
-            }
-        } 
-        
-        if (inputMode === 'loginPassword') {
-            const tempLoginPassword = message
-
-            if (logins[clients[clientAddress].username]?.password === tempLoginPassword) {
-                clients[clientAddress].inputMode = 'chCrSpecies'
-                console.log(clients)
-                ws.send("It's your first time logging in. Please create a character.")
-                ws.send(`Begin by choosing your character species.<br><br>
-                         1. Human <br>2. Elf <br>3. Dwarf <br>4. Halfling <br>5. Halfgiant`)
-
-            } 
-            
-            else {
-                if (loginPasswordAttempts === 0) {
-                    clients[clientAddress].inputMode = 'loginOrRegister'
-                    ws.send(':lightpurple: Login failed. Please login or register.')
-                    loginPasswordAttempts = 2
-                }
-
-                else {
-                    ws.send('Incorrect password. Try again.')
-                    --loginPasswordAttempts
-                }
-            }
-        } 
-        
-        if (inputMode === 'registerUsername') {
-            const tempRegisterUsername = message
-            
-            if (logins[tempRegisterUsername]) {
-                ws.send('Username already exists. Please choose a different username.')
-            } 
-            
-            else {
-                ws.send(`Your username is "${message}"`)
-                clients[clientAddress].inputMode = 'registerPassword'
-                clients[clientAddress].tempRegisterUsername = tempRegisterUsername
-                ws.send('Please register your password.')
-            }
-            
-        } 
-        
-        if (inputMode === 'registerPassword') {
-            const tempRegisterPassword = message
-            clients[clientAddress].inputMode = 'registerPasswordConfirm'
-            clients[clientAddress].tempRegisterPassword = tempRegisterPassword
-            ws.send('Please confirm your password.')
-        } 
-        
-        if (inputMode === 'registerPasswordConfirm') {
-            const { tempRegisterUsername, tempRegisterPassword } = clients[clientAddress]
-
-            if (message === tempRegisterPassword) {
-                logins[tempRegisterUsername] = {
-                    netAddresses: logins[tempRegisterUsername]?.netAddresses || [],
-                    password: tempRegisterPassword,
-                    character: ''
-                }
-
-                logins[tempRegisterUsername].netAddresses.push(clientAddress)
-                fs.writeFileSync('database/logins.json', JSON.stringify(logins), 'utf8')
-                clients[clientAddress].tempRegisterUsername = ''
-                clients[clientAddress].tempRegisterPassword = ''
-                clients[clientAddress].inputMode = 'loginOrRegister'
-                ws.send('Registration successful. Proceed to login.')
-                console.log(logins)
-            } 
-            
-            else {
-                if (registerPasswordAttempts === 0) {
-                    clients[clientAddress].inputMode = 'loginOrRegister'
-                    ws.send(':lightpurple: Registration failed. Please login or register.')
-                    registerPasswordAttempts = 2
-                }
-
-                else {ws.send('Passwords do not match. <br>Please register your password.')
-                --registerPasswordAttempts
-                clients[clientAddress].inputMode = 'registerPassword'
-                }
-            }
-        }
-    })
+      this.handleMessage(ws, clientAddress, message);
+    });
 
     ws.on('close', () => {
-        const clientAddress = ws._socket.remoteAddress + ':' + ws._socket.remotePort
-        console.log(`${clientAddress} disconnected.`)
-        delete clients[clientAddress]
-        console.log('Number of connected clients:', Object.keys(clients).length)
-    })
+      this.handleClose(clientAddress);
+    });
 
     ws.on('error', (error) => {
-        console.error('WebSocket error:', error)
-    })
-})
+      console.error('WebSocket error:', error);
+    });
+  }
 
-console.log('Server is online.')
+  handleMessage(ws, clientAddress, message) {
+    const { inputMode } = this.clients[clientAddress];
+    message = message.toString().trim();
+    console.log(`Received message: ${message}`);
+
+    if (inputMode === 'loginOrRegister') {
+      if (message === 'l') {
+        this.clients[clientAddress].inputMode = 'loginUsername';
+        ws.send('Please enter your username.');
+      } else if (message === 'r') {
+        this.clients[clientAddress].inputMode = 'registerUsername';
+        ws.send('Please register your username.');
+      } else {
+        ws.send('Type l or r to login or register.');
+      }
+    }
+
+    if (inputMode === 'loginUsername') {
+      const tempLoginUsername = message;
+
+      if (this.logins[tempLoginUsername]) {
+        this.clients[clientAddress].inputMode = 'loginPassword';
+        this.clients[clientAddress].username = tempLoginUsername;
+        ws.send('Please enter your password.');
+      } else {
+        if (this.loginUsernameAttempts === 0) {
+          this.clients[clientAddress].inputMode = 'loginOrRegister';
+          ws.send(':lightpurple: Login failed. Please try again.');
+          this.loginUsernameAttempts = 2;
+        } else {
+          ws.send('User not found.');
+          --this.loginUsernameAttempts;
+        }
+      }
+    }
+
+    if (inputMode === 'loginPassword') {
+      const tempLoginPassword = message;
+
+      if (this.logins[this.clients[clientAddress].username]?.password === tempLoginPassword) {
+        this.clients[clientAddress].inputMode = 'chCrSpecies';
+        console.log(this.clients);
+        ws.send("It's your first time logging in. Please create a character.");
+        ws.send(`Begin by choosing your character species.<br><br>
+                         1. Human <br>2. Elf <br>3. Dwarf <br>4. Halfling <br>5. Halfgiant`);
+      } else {
+        if (this.loginPasswordAttempts === 0) {
+          this.clients[clientAddress].inputMode = 'loginOrRegister';
+          ws.send(':lightpurple: Login failed. Please login or register.');
+          this.loginPasswordAttempts = 2;
+        } else {
+          ws.send('Incorrect password. Try again.');
+          --this.loginPasswordAttempts;
+        }
+      }
+    }
+
+    if (inputMode === 'registerUsername') {
+      const tempRegisterUsername = message;
+
+      if (this.logins[tempRegisterUsername]) {
+        ws.send('Username already exists. Please choose a different username.');
+      } else {
+        ws.send(`Your username is "${message}"`);
+        this.clients[clientAddress].inputMode = 'registerPassword';
+        this.clients[clientAddress].tempRegisterUsername = tempRegisterUsername;
+        ws.send('Please register your password.');
+      }
+    }
+
+    if (inputMode === 'registerPassword') {
+      const tempRegisterPassword = message;
+      this.clients[clientAddress].inputMode = 'registerPasswordConfirm';
+      this.clients[clientAddress].tempRegisterPassword = tempRegisterPassword;
+      ws.send('Please confirm your password.');
+    }
+
+    if (inputMode === 'registerPasswordConfirm') {
+      const { tempRegisterUsername, tempRegisterPassword } = this.clients[clientAddress];
+
+      if (message === tempRegisterPassword) {
+        this.logins[tempRegisterUsername] = {
+          netAddresses: this.logins[tempRegisterUsername]?.netAddresses || [],
+          password: tempRegisterPassword,
+          character: '',
+        };
+
+        this.logins[tempRegisterUsername].netAddresses.push(clientAddress);
+        fs.writeFileSync('database/logins.json', JSON.stringify(this.logins), 'utf8');
+        this.clients[clientAddress].tempRegisterUsername = '';
+        this.clients[clientAddress].tempRegisterPassword = '';
+        this.clients[clientAddress].inputMode = 'loginOrRegister';
+        ws.send('Registration successful. Proceed to login.');
+        console.log(this.logins);
+      } else {
+        if (this.registerPasswordAttempts === 0) {
+          this.clients[clientAddress].inputMode = 'loginOrRegister';
+          ws.send(':lightpurple: Registration failed. Please login or register.');
+          this.registerPasswordAttempts = 2;
+        } else {
+          ws.send('Passwords do not match. <br>Please register your password.');
+          --this.registerPasswordAttempts;
+          this.clients[clientAddress].inputMode = 'registerPassword';
+        }
+      }
+    }
+  }
+
+  handleClose(clientAddress) {
+    console.log(`${clientAddress} disconnected.`);
+    delete this.clients[clientAddress];
+    console.log('Number of connected clients:', Object.keys(this.clients).length);
+  }
+}
+
+new Server();
