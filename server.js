364ea508fe702dc3b4cd61c10cd0ac11c89e2d2f
l3g3nd4ndm7th <l3g3nd4ndm7th@gmail.com>
2023-07-17 14:29:21 -0400

    added some additional character creation process and changed from class encapsulation to function encapsulation

--- server.js
+++ server.js
@@ -5,9 +5,6 @@ function startServer() {
     let clients = {};
     let logins = {};
     let characters = {};
-    let loginUsernameAttempts = 2;
-    let loginPasswordAttempts = 2;
-    let registerPasswordAttempts = 2;
 
     // Read logins data from file
     try {
@@ -28,20 +25,23 @@ function startServer() {
     const wss = new WebSocket.Server({ port: 8080 });
 
     wss.on('connection', (ws, req) => {
-        const clientAddress = req.socket.remoteAddress + ':' + req.socket.remotePort;
-        console.log(`${clientAddress} has connected.`);
+        ws.clientAddress = req.socket.remoteAddress + ':' + req.socket.remotePort;
+        const { clientAddress } = ws;
+        console.log(`\n${clientAddress} connected.`);
         ws.send(':lightpurple: Connected to the server.');
         ws.send(':lightpurple: Please login or register.');
 
         clients[clientAddress] = {
             username: '',
             inputMode: 'loginOrRegister',
-            characterName: '',
-            abilityScoresRerolls: 3,
-            timeAbilityScoresReroll: 0
+            loginUsernameAttempts: 2,
+            loginPasswordAttempts: 2,
+            registerUsernameAttempts: 2,
+            registerPasswordAttempts: 2
         };
 
         console.log('Number of connected clients:', Object.keys(clients).length);
+        console.log(Object.keys(clients), '\n');
 
         ws.on('message', (message) => {
             const { inputMode } = clients[clientAddress];
@@ -69,13 +69,13 @@ function startServer() {
                     clients[clientAddress].username = tempLoginUsername;
                     ws.send('Please enter your password.');
                 } else {
-                    if (loginUsernameAttempts === 0) {
+                    if (clients[clientAddress].loginUsernameAttempts === 0) {
                         clients[clientAddress].inputMode = 'loginOrRegister';
                         ws.send(':lightpurple: Login failed. Please try again.');
-                        loginUsernameAttempts = 2;
+                        clients[clientAddress].loginUsernameAttempts = 2;
                     } else {
                         ws.send('User not found.');
-                        --loginUsernameAttempts;
+                        --clients[clientAddress].loginUsernameAttempts;
                     }
                 }
             }
@@ -85,6 +85,7 @@ function startServer() {
 
                 if (logins[clients[clientAddress].username]?.password === tempLoginPassword) {
                     clients[clientAddress].inputMode = 'chCrSpecies';
+                    console.log(`${clientAddress} logged in.`)
                     console.log(clients[clientAddress]);
 
                     if (!characters[clients[clientAddress].username]) {
@@ -105,20 +106,24 @@ function startServer() {
                         };
 
                         characters[clients[clientAddress].username] = character;
-                        console.log(`Character created for ${clients[clientAddress].username}`);
-                        ws.send("Please create a character.\n\n");
-                        ws.send("Begin by choosing your character species.\n\n1. Human\n2. Elf\n3. Dwarf");
+                        ws.send("Please create a character.<br><br>");
+                        ws.send("Choose your species.<br><br>(h) Human<br>(e) Elf<br>(d) Dwarf");
                     } else {
                         ws.send(`Welcome to the game world, ${characters[clients[clientAddress].username].name}.`);
+
+                        if (!logins[clients[clientAddress].username]?.netAddresses.includes(clientAddress)) {
+                            logins[clients[clientAddress].username]?.netAddresses.push(clientAddress);
+                        }
                     }
+
                 } else {
-                    if (loginPasswordAttempts === 0) {
+                    if (clients[clientAddress].loginPasswordAttempts === 0) {
                         clients[clientAddress].inputMode = 'loginOrRegister';
                         ws.send(':lightpurple: Login failed. Please login or register.');
-                        loginPasswordAttempts = 2;
+                        clients[clientAddress].loginPasswordAttempts = 2;
                     } else {
                         ws.send('Incorrect password. Try again.');
-                        --loginPasswordAttempts;
+                        --clients[clientAddress].loginPasswordAttempts;
                     }
                 }
             }
@@ -127,7 +132,14 @@ function startServer() {
                 const tempRegisterUsername = message;
 
                 if (logins[tempRegisterUsername]) {
-                    ws.send('Username already exists. Please choose a different username.');
+                    if (clients[clientAddress].loginUsernameAttempts === 0) {
+                        clients[clientAddress].inputMode = 'loginOrRegister';
+                        ws.send(':lightpurple: Registration failed. Please login or register.');
+                        clients[clientAddress].loginUsernameAttempts = 2;
+                    } else {
+                        ws.send('Username already exists. Please choose a different username.');
+                        --clients[clientAddress].registerUsernameAttempts;
+                    }
                 } else {
                     ws.send(`Your username is "${message}"`);
                     clients[clientAddress].inputMode = 'registerPassword';
@@ -148,9 +160,8 @@ function startServer() {
 
                 if (message === tempRegisterPassword) {
                     logins[tempRegisterUsername] = {
-                        netAddresses: logins[tempRegisterUsername]?.netAddresses || [],
-                        password: tempRegisterPassword,
-                        character: ''
+                        netAddresses: [],
+                        password: tempRegisterPassword
                     };
 
                     logins[tempRegisterUsername].netAddresses.push(clientAddress);
@@ -161,13 +172,13 @@ function startServer() {
                     clients[clientAddress].inputMode = 'loginOrRegister';
                     ws.send('Registration successful. Proceed to login.');
                 } else {
-                    if (registerPasswordAttempts === 0) {
+                    if (clients[clientAddress].registerPasswordAttempts === 0) {
                         clients[clientAddress].inputMode = 'loginOrRegister';
                         ws.send(':lightpurple: Registration failed. Please login or register.');
-                        registerPasswordAttempts = 2;
+                        clients[clientAddress].registerPasswordAttempts = 2;
                     } else {
                         ws.send('Passwords do not match. Please register your password.');
-                        --registerPasswordAttempts;
+                        --clients[clientAddress].registerPasswordAttempts;
                         clients[clientAddress].inputMode = 'registerPassword';
                     }
                 }
@@ -175,33 +186,99 @@ function startServer() {
 
             if (inputMode === 'chCrSpecies') {
                 if (message === 'h' || message === 'human') {
-                    // Handle Human species
+                    const species = 'Human';
+                    characters[clients[clientAddress].username].species = species;
+                    ws.send(`You chose ${species}.<br><br>`);
+                    clients[clientAddress].inputMode = 'chCrArchetype'
+                    ws.send("Choose your archetype.<br><br>(f) Fighter<br>(r) Rogue<br>(m) Mage");
                 } else if (message === 'e' || message === 'elf') {
-                    // Handle Elf species
+                    const species = 'Elf';
+                    characters[clients[clientAddress].username].species = species;
+                    ws.send(`You chose ${species}.<br><br>`);
+                    clients[clientAddress].inputMode = 'chCrArchetype'
+                    ws.send("Choose your archetype.<br><br>(f) Fighter<br>(r) Rogue<br>(m) Mage");
                 } else if (message === 'd' || message === 'dwarf') {
-                    // Handle Dwarf species
+                    const species = 'Dwarf';
+                    characters[clients[clientAddress].username].species = species;
+                    ws.send(`You chose ${species}.<br><br>`);
+                    clients[clientAddress].inputMode = 'chCrArchetype'
+                    ws.send("Choose your archetype.<br><br>(f) Fighter<br>(r) Rogue<br>(m) Mage");
+                }
+            }
+
+            if (inputMode === 'chCrArchetype') {
+                if (message === 'f' || message === 'fighter') {
+                    const archetype = 'Fighter';
+                    characters[clients[clientAddress].username].archetype = archetype;
+                    ws.send(`You chose ${archetype}.<br><br>`);
+                    clients[clientAddress].inputMode = 'chCrAlignment'
+                    ws.send("Choose your alignment.<br><br>(l) Lawful<br>(n) Neutral<br>(c) Chaotic");
+                } else if (message === 'r' || message === 'rogue') {
+                    const archetype = 'Rogue';
+                    characters[clients[clientAddress].username].archetype = archetype;
+                    ws.send(`You chose ${archetype}.<br><br>`);
+                    clients[clientAddress].inputMode = 'chCrAlignment'
+                    ws.send("Choose your alignment.<br><br>(l) Lawful<br>(n) Neutral<br>(c) Chaotic");
+                } else if (message === 'm' || message === 'mage') {
+                    const archetype = 'Mage';
+                    characters[clients[clientAddress].username].archetype = archetype;
+                    ws.send(`You chose ${archetype}.<br><br>`);
+                    clients[clientAddress].inputMode = 'chCrAlignment'
+                    ws.send("Choose your alignment.<br><br>(l) Lawful<br>(n) Neutral<br>(c) Chaotic");
+                }
+            }
+
+            if (inputMode === 'chCrAlignment') {
+                if (message === 'l' || message === 'lawful') {
+                    const alignment = 'Lawful';
+                    characters[clients[clientAddress].username].alignment = alignment;
+                    ws.send(`You chose ${alignment}.<br><br>`);
+                    clients[clientAddress].inputMode = 'chCrName'
+                    ws.send("Enter your character's name.");
+                } else if (message === 'n' || message === 'neutral') {
+                    const alignment = 'Neutral';
+                    characters[clients[clientAddress].username].alignment = alignment;
+                    ws.send(`You chose ${alignment}.<br><br>`);
+                    clients[clientAddress].inputMode = 'chCrName'
+                    ws.send("Enter your character's name.");
+                } else if (message === 'c' || message === 'chaotic') {
+                    const alignment = 'Chaotic';
+                    characters[clients[clientAddress].username].alignment = alignment;
+                    ws.send(`You chose ${alignment}.<br><br>`);
+                    clients[clientAddress].inputMode = 'chCrName'
+                    ws.send("Enter your character's name.");
+                }
+            }
+
+            if (inputMode === 'chCrName') {
+                if (message != '') {
+                    characters[clients[clientAddress].username].name = message
+                    ws.send(`Your character's name is ${message}`)
+                    console.log(`Character created for ${clients[clientAddress].username}`);
+                    console.log(characters[clients[clientAddress].username])
                 }
             }
         });
-
+        
         ws.on('close', () => {
-            console.log(`${clientAddress} disconnected.`);
+            console.log(`\n${clientAddress} disconnected.`);
             delete clients[clientAddress];
             console.log('Number of connected clients:', Object.keys(clients).length);
+            console.log(Object.keys(clients), '\n');
         });
 
         ws.on('error', (error) => {
             console.error('WebSocket error:', error);
         });
-    })
-    
+    });
+
     process.on('SIGINT', () => {
         console.log('Cleaning up and exiting gracefully...');
         fs.writeFileSync('database/logins.json', JSON.stringify(logins), 'utf8');
         fs.writeFileSync('database/characters.json', JSON.stringify(characters), 'utf8');
         process.exit();
     });
-    
+
     console.log('Server is online.');
 }
 
